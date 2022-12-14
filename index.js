70de3eb4dc39e49fbf95f49a9d1b03c65ec4381e
Florian <Florianlebecque@hotmail.com>
2022-12-14 11:51:48 +0100

    change port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ var Gun = require('gun');
 var gun = Gun({web: server});
 
 // Start the server on port 8080.
-server.listen(8080, function () {
+server.listen(5254, function () {
   console.log('Server listening on http://localhost:8080/gun')
 })
